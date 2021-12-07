c5621c7ae195e7ddb59e4fcbf55e2a66b4f7ee1a
Jude Alistair <jude55593@gmail.com>
2021-12-07 15:28:56 +0100

    working progress

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,30 +1,38 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, AfterViewInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements AfterViewInit {
+
+  @Input() title!: string;
+  @Input() body!: string;
 
   @ViewChild('truncator') truncator!: ElementRef<HTMLElement>;
   @ViewChild('bodyText') bodyText!: ElementRef<HTMLElement>;
 
   constructor(private renderer: Renderer2) { }
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
+
+  // }
+
+
+  ngAfterViewInit() :void {
     //work out if there is a text overflow and if not, then hide the truncator
 
-    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let style = window.getComputedStyle(this.bodyText!.nativeElement, null);
     let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
 
-    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+    if(this.bodyText!.nativeElement.scrollHeight > viewableHeight) {
       // if there is a text overflow, show the fade out truncator
-      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+      this.renderer.setStyle(this.truncator?.nativeElement, 'display', 'block');
     } else {
       // else (there is a text overflow), hide the fade out truncator
-      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+      this.renderer.setStyle(this.truncator?.nativeElement, 'display', 'none');
     }
   }
-
 }
+
